a0a028ed093ebd56adce2bc77df1b4ba81800771
Nikita Kurilovich <nik.kurilovich.06@gmail.com>
2026-05-31 00:13:19 +0300

    fix: clear extra

--- app/classifier.py
+++ app/classifier.py
@@ -1,3 +1,5 @@
+
+
 class Feature:
     def __init__ (self, name: str, keywords: list[str], weight: int):
         self.name = name
@@ -26,20 +28,19 @@ class Feature:
 class Category:
     def __init__(self, name: str, features: list[Feature]):
         self.categoryName=name
-        self.categoryFeatures=features
+        self.category_features=features
     
     def get_total_score_normalized(self, data: str)->float:
-        totalScore=0
-        sum_weights=sum(abs(feature.weight) for feature in self.categoryFeatures)
-        for feature in self.categoryFeatures:
-            totalScore += feature.get_score(data)
+        total_score=0
+        sum_weights=sum(abs(feature.weight) for feature in self.category_features)
+        for feature in self.category_features:
+            total_score += feature.get_score(data)
         
-        return totalScore/sum_weights
+        return total_score/sum_weights
 
 
 class MailClassifier:
-    
-    def __init__(self, categories: list[Category], minScore: float):
+    def __init__(self,categories: list[Category], minScore: float):
         self.categories = categories
         self.minScore=minScore
     
@@ -54,42 +55,8 @@ class MailClassifier:
         if bestScore<self.minScore:
             return "needs_review"
         
-        return [bestCategory, scoresOfCategories] #потом поменять - оставить только bestCategory
+        return bestCategory 
 
-
-
-#ниже пока что для примера - потом будет подгружаться из других файлов
-if __name__ == "__main__":
-
-    spam_features = [
-        Feature("срочность", ["срочно", "urgent"], 1),
-        Feature("фишинг", ["логин", "пароль", "банковская карта"], 2),
-        Feature("выигрыш", ["вы выиграли", "приз", "подарок"], 1),
-        Feature("штрафные слова", ["не работает", "уже второй день", "затронуты", "коллег"], -1)
-    ]
-
-    important_features = [
-        Feature("важность", ["важно", "important"], 3),
-        Feature("массовость", ["не работает", "уже второй день", "затронуты", "коллег"], 2),
-    ]
-
-    spam_category = Category("spam_phishing", spam_features)
-    important_category = Category("important_category", important_features)
-
-    classifier = MailClassifier([spam_category,important_category],0.1)
-
-    text1 = "Здравствуйте! Срочно требуется ваш логин, это важно, ничего не работает"
-    text2 = "Добрый день, как дела?"
-    
-    print()
-
-    print(text1)
-    print(classifier.classify(text1))
-    print()
-
-    print(text2)
-    print(classifier.classify(text2))
-    print()
 
 
 
