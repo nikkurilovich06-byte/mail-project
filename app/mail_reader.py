a70e7ad18695c179ea793f61a81e8e624113fac9
vlad_seledkin <vlseseledkin@edu.hse.ru>
2026-05-30 22:15:29 +0300

    feat: add method _parse

--- app/mail_reader.py
+++ app/mail_reader.py
@@ -28,6 +28,33 @@ class MailReader:
 
         raw_file = file_path.read_bytes()
         encoding = chardet.detect(raw_file)['encoding'] or 'utf-8'
-        content = raw_file.decode(encoding, errors='replace')
+        content = raw.decode(encoding, errors='replace')
 
         return self._parse(file_path.name, content)
+
+    def _parse(self, filename: str, content: str) -> MailMessage:
+        lines = content.splitlines()
+        subject = ""
+        sender = ""
+        body = []
+        in_body = False
+
+        subject_keys = ["Subject:", "Тема:"]
+        sender_keys = ["From:", "От кого:"]
+
+        for line in lines:
+            if in_body:
+                body.append(line)
+            elif any(line.startswith(key) for key in subject_keys):
+                subject = line.split(":", 1)[1].strip()
+            elif any(line.startswith(key) for key in sender_keys):
+                sender = line.split(":", 1)[1].strip()
+            elif line == "":
+                in_body = True
+
+        return MailMessage(
+            filename=filename,
+            subject=subject,
+            sender=sender,
+            body="\n".join(body)
+        )
