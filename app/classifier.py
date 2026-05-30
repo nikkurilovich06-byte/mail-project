3adbe4f2e5ca077dfff957a80b61d515d0793d18
Nikita Kurilovich <nik.kurilovich.06@gmail.com>
2026-05-30 19:09:41 +0300

    feat: add class MailClassifier

--- app/classifier.py
+++ app/classifier.py
@@ -17,17 +17,48 @@ class Feature:
             return self.weight
         return 0
 
+
 class Category:
-    def __init__(self, name: str, features: list[Feature]):
+    def __init__(self, name: str, features: list[Feature], maxScore: int):
         self.categoryName=name
         self.categoryFeatures=features
+        self.maxScore=maxScore
     
-    def get_total_score(self, data: str)->int:
+    def get_total_score_normalized(self, data: str)->int:
         totalScore=0
         for feature in self.categoryFeatures:
             totalScore += feature.get_score(data)
-        return totalScore
+        return totalScore/self.maxScore
 
+
+class MailClassifier:
+    def __init__(self, categories: list[Category], minScore: float):
+        self.categories = categories
+        self.minScore=0.1
+    
+    def classify(self, data: str)-> str:
+        scoresOfCategories={}
+        for category in self.categories:
+            scoresOfCategpries[category.name] = category.get_score(data)
+        
+        bestCategory = max(scoresOfCategory, key=scoresOfCategory.get(0))
+        bestScore = scoresOfCategory[bestCategory]
+
+        if bestScore<minScore:
+            return "needs_review"
+        
+        return bestCategory
+
+
+
+
+
+
+
+
+
+
+        
 
 
 
