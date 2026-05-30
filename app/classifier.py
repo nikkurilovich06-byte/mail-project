4c35ceef9e9229ff114e6e01eac55f3e342a165b
Nikita Kurilovich <nik.kurilovich.06@gmail.com>
2026-05-30 18:42:51 +0300

    feat: add class Feature to classifier

--- app/classifier.py
+++ app/classifier.py
@@ -1,7 +1,22 @@
 
 class Feature:
-    def __init__(self, name: str, keywords: list[str], max_weight: int):
+    def __init__(self, name: str, keywords: list[str], weight: int):
         self.name = name
         self.keywords = keywords
-        self.max_weight = max_weight
+        self.weight = weight
+    
+    def word_found(self, data:str) -> bool:
+
+        for keyword in self.keywords:
+            if keyword in text:
+                return True
+        return False
+
+
+    def get_score(self, data: str) -> float:
+        if self.word_found(data):
+            return self.weight
+        return 0
+
+
     
