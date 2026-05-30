ee04d550b8787168c5e05d41798f9616965651ae
Nikita Kurilovich <nik.kurilovich.06@gmail.com>
2026-05-30 17:34:56 +0300

    feat: add initial class to classifier

---
+++ app/classifier.py
@@ -0,0 +1,7 @@
+
+class Feature:
+    def __init__(self, name: str, keywords: list[str], max_weight: int):
+        self.name = name
+        self.keywords = keywords
+        self.max_weight = max_weight
+    
