10f35c71a6d685a63b6ccb9df3308cd8d1a8f09f
Зиля <gizatullinazila64@gmail.com>
2026-05-31 12:09:20 +0300

    feat: add coments in logger_config.py

--- app/logger_config.py
+++ app/logger_config.py
@@ -1,14 +1,14 @@
-import logging
-import os
+import logging               #подключаем библиотеку для логирования
+import os                     #подключаем библиотеку для работы с папками
 
 def setup_logger():
-    
-    os.makedirs("logs", exist_ok=True)
 
-    logging.basicConfig(
-        filename = "logs/event.log",
-        level = logging.INFO,
-        format = "%(asctime)s | %(levelname)s | %(message)s", 
-        ecoding = "utf-8"
+    os.makedirs("logs", exist_ok=True)       #создается папка logs, если её нет
+
+    logging.basicConfig(                                       #настройка логирования
+        filename = "logs/event.log",                            #указываем, где будем хранить логи
+        level = logging.INFO,                                    #записываем сообщения уровня INFO и выше (INFO, WARNING, ERROR, CRITICAL)
+        format = "%(asctime)s | %(levelname)s | %(message)s",     #шаблон для сообщений (дата | уровень | сообщение)
+        encoding = "utf-8"                                         #файл должен поодерживать русский текст
     )
 
