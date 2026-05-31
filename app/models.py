f00a8dba5b38b1fb03b221f8fc4f97f7c5432270
Зиля <gizatullinazila64@gmail.com>
2026-05-31 15:43:59 +0300

    feat: removed method addTotal and add comments in models.py

--- app/models.py
+++ app/models.py
@@ -1,32 +1,32 @@
-from pathlib import Path       # подключаем библиотеку для рботы с путями к файлам
+from pathlib import Path          # подключаем библиотеку для работы с путями к файлам
 
 class MailMessage:
     def __init__(self, filename: str, path: Path, content: str, subject: str, sender: str, recipient: str):
-        self.filename = filename          # имя файла
-        self.path = path                  # путь к файлу
-        self.content = content            # содержание сообщения
-        self.subject = subject            # тема сообщения
-        self.sender =  sender             # отправитель
-        self.recipient = recipient        # получатель
+        self.filename = filename       # имя файла
+        self.path = path               # путь к файлу
+        self.content = content         # содержимое письма
+        self.subject = subject         # тема письма
+        self.sender = sender           # отправитель письма
+        self.recipient = recipient     # получатель письма
 
 class ProcessingStats:
     def __init__(self):
-        self.total = 0                    # общее количество обработанных писем
-        self.processed = 0                # количество удачно обработанных писем
-        self.broken = 0                   # количество сломаных писем
-        self.categories = {}              # сколько писем попало в каждую категорию
+        self.total = 0                 # общее количество обработанных писем
+        self.processed = 0             # количество успешно обработанных писем
+        self.broken = 0                # количество сломанных писем
+        self.categories = {}           # словарь для хранения количества писем в каждой категории
 
-    def addTotal(self):
-        self.total += 1
-    
     def addProcessed(self):
         self.processed += 1
+        self.total += 1
     
     def addBroken(self):
         self.broken += 1
+        
 
     def addToCategory(self, category: str):
-        if category in self.categories:         
-            self.categories[category] += 1      #если категория уже есть, то увеличиваем её счетчик на 1
-        else:
-            self.categories[category] = 1       #ели категории нет, то создаем её и присваиваем значение 1
+        if category in self.categories:        # если категория уже есть в словаре, то увеличиваем значение на 1
+            self.categories[category] += 1
+        else:                                  #если категории нет в словаре, то добавляем ее и присваиваем значение 1
+            self.categories[category] = 1
+ gi
